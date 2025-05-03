be626ac258adaec822ccd0fcb31d1e30e41208bb
Ahmed Abaza <a.abaza90@gmail.com>
2025-05-03 12:06:44 +0300

    2

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./assets/i18n";
+
 import theme from "./components/theme";
 
 const root = ReactDOM.createRoot(
@@ -15,9 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
